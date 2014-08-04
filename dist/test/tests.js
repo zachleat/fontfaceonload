45ad5b329d498841e5381bbb4bd8eb11f9809240
Zach Leatherman <zachleatherman@gmail.com>
2014-08-04 14:10:07 -0500

    fontfaceonload 0.0.1 [ci skip]

---
+++ dist/test/tests.js
@@ -0,0 +1,16 @@
+(function( w, $ ) {
+	"use strict";
+
+	module( "Defaults", {
+		setup: function() {
+			
+		},
+		teardown: function() {
+			
+		}
+	});
+
+	test( "True", function() {
+		ok( true );
+	});
+}( this, jQuery ));
