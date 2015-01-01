c1b4e38aa575f2ae3c3ebfabc6bea13d67868e7e
Zach Leatherman <zachleatherman@gmail.com>
2015-01-01 12:19:06 -0600

    First attempt at fixing #13. Note that it is not trivial to know what font weight has been loaded. Also, be warned that faux-bolding will make the script think a bold weight has loaded when it has not.

--- src/fontfaceonload.js
+++ src/fontfaceonload.js
@@ -8,6 +8,7 @@
 		defaultOptions = {
 			tolerance: 2, // px
 			delay: 100,
+			weights: 1, // total number of font-weights
 			glyphs: '',
 			success: function() {},
 			error: function() {},
@@ -89,6 +90,40 @@
 			return ( new Date() ).getTime() - startTime.getTime() > options.timeout;
 		}
 
+		function checkWeights() {
+			var j, k,
+				test = [ 400, 600, 700, 300, 500, 800, 900, 100, 200 ],
+				weights = [];
+
+			for( j = 0, k = test.length; j <= k; j++ ) {
+				dimensions = that.getMeasurements();
+				sansSerif.style.fontWeight = serif.style.fontWeight = test[ j ];
+
+				if( hasNewDimensions( dimensions.sansSerif, sansSerif, 0 ) ||
+						hasNewDimensions( dimensions.serif, serif, 0 ) ) {
+
+					weights.push( test [ j ] );
+
+					if( weights.length >= options.weights ) {
+						break;
+					}
+				}
+			}
+
+			if( weights.length >= options.weights ) {
+				for( j = 0, k = weights.length; j < k; j++ ) {
+					options.success();
+				}
+				ref.removeChild( parent );
+			} else if( isTimeout() ) {
+				options.error();
+			} else {
+				win.setTimeout(function() {
+					checkWeights();
+				}, options.delay );
+			}
+		}
+
 		(function checkDimensions() {
 			if( !ref ) {
 				ref = doc.body;
@@ -110,7 +145,12 @@
 				( hasNewDimensions( dimensions.sansSerif, sansSerif, options.tolerance ) ||
 					hasNewDimensions( dimensions.serif, serif, options.tolerance ) ) ) {
 
-				options.success();
+				// short circuit if only one weight, we already know the dimensions changed.
+				if( options.weights === 1 ) {
+					options.success();
+				} else {
+					checkWeights();
+				}
 			} else if( isTimeout() ) {
 				options.error();
 			} else {
@@ -124,16 +164,29 @@
 	}; // end load()
 
 	FontFaceOnloadInstance.prototype.checkFontFaces = function( timeout ) {
-		var _t = this;
+		var _t = this,
+			checkCount = 0,
+			weights = {};
 
 		doc.fonts.forEach(function( font ) {
 			if( font.family.toLowerCase() === _t.fontFamily.toLowerCase() ) {
+				if( !weights[ font.weight ] ) {
+					checkCount++;
+					weights[ font.weight ] = true;
+				}
+
 				font.load().then(function() {
 					_t.options.success();
 					win.clearTimeout( timeout );
 				});
 			}
 		});
+
+		if( checkCount !== _t.options.weights ) {
+			win.setTimeout(function() {
+				_t.checkFontFaces( timeout );
+			}, _t.options.delay );
+		}
 	};
 
 	FontFaceOnloadInstance.prototype.init = function( fontFamily, options ) {
